797eaec14f44865e180f4b94c2529d3a6aedc481
Nadezhda <dachkovskanadezhda@gmail.com>
2021-12-03 23:34:11 +0900

    Боковое навигационное меню

--- app/src/main/java/ru/dachkovska/notes/MainActivity.java
+++ app/src/main/java/ru/dachkovska/notes/MainActivity.java
@@ -1,12 +1,16 @@
 package ru.dachkovska.notes;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initToolbar();
+        initToolbarAndDrawer();
 
         // Создаем фрагмент
         NoteFragment notesFragment = new NoteFragment();
@@ -26,9 +30,49 @@ public class MainActivity extends AppCompatActivity {
                 .add(R.id.fragment_container, notesFragment).commit();
     }
 
-    private void initToolbar() {
+    private void initToolbarAndDrawer() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        initDrawer(toolbar);
+    }
+    private void initDrawer(Toolbar toolbar) {
+
+        // Находим DrawerLayout
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        // Создаем ActionBarDrawerToggle
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+        toggle.syncState();
+
+        // Обработка навигационного меню
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.action_drawer_about:
+                        openAboutFragment();
+                        drawer.closeDrawers();
+                        return true;
+                    case R.id.action_drawer_exit:
+                        finish();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void openAboutFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack("")
+                .add(R.id.fragment_container, new AboutFragment()).commit();
     }
 
     @Override
@@ -42,10 +86,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
         switch (id) {
             case R.id.action_about:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .addToBackStack("")
-                        .add(R.id.fragment_container, new AboutFragment()).commit();
+                openAboutFragment();
                 return true;
             case R.id.action_exit:
                 finish();
