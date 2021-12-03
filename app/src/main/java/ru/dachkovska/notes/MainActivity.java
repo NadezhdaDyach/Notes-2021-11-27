064201689b8c27d4b5decd2f758745b50b1f19bc
Nadezhda <dachkovskanadezhda@gmail.com>
2021-12-03 23:11:15 +0900

    Создали собственный тулбар

--- app/src/main/java/ru/dachkovska/notes/MainActivity.java
+++ app/src/main/java/ru/dachkovska/notes/MainActivity.java
@@ -1,8 +1,12 @@
 package ru.dachkovska.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
 
         // Создаем фрагмент
         NoteFragment notesFragment = new NoteFragment();
@@ -19,4 +25,34 @@ public class MainActivity extends AppCompatActivity {
                 .addToBackStack("")
                 .add(R.id.fragment_container, notesFragment).commit();
     }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_about:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("")
+                        .add(R.id.fragment_container, new AboutFragment()).commit();
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
